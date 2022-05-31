f0d50dff2823216dfd08e0a22b342b01c1f4d402
Lucas Tavares <lucas.tavares@petzpartner.com.br>
2022-05-30 23:16:30 -0300

    Projeto criado

---
+++ src/main/java/com/lucasrtavares/PrimeirocursoApplication.java
@@ -0,0 +1,13 @@
+package com.lucasrtavares;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeirocursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeirocursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasrtavares/PrimeirocursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasrtavares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeirocursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
