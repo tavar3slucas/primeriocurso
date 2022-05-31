9a48eadf44729bfcd96b6c84e3c28242a578632c
Lucas Tavares <lucas.tavares@petzpartner.com.br>
2022-05-31 15:25:03 -0300

    Criando operação de instanciação

--- src/main/java/com/lucasrtavares/PrimeirocursoApplication.java
+++ src/main/java/com/lucasrtavares/PrimeirocursoApplication.java
@@ -1,12 +1,31 @@
 package com.lucasrtavares;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class PrimeirocursoApplication {
+import com.lucasrtavares.domain.Categoria;
+import com.lucasrtavares.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class PrimeirocursoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PrimeirocursoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		repo.saveAll(Arrays.asList(cat1,cat2));
 	}
 }
